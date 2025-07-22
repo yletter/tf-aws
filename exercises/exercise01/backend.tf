b407739deace351945e9a0a3325c6e09bc2e3fe2
yuvarajengineer <yuvarajengineer@gmail.com>
2025-07-22 23:15:23 +0530

    Update backend.tf

--- exercises/exercise01/backend.tf
+++ exercises/exercise01/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "tf-state-yuvaraj"
     region = "us-east-1"
-    key = "exercise0/terraform.tfstate"
+    key = "exercise01/terraform.tfstate"
     #profile = "saml"
   }
 }
