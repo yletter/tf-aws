1973ee8ebd068e6b2838fd861b1474c550e9ddf1
yuvarajengineer@gmail.com <yuvarajengineer@gmail.com>
2025-07-30 08:00:10 +0530

    Revert "Update main.tf"
    This reverts commit f10a8a3f296257407e8849a0117ed254a8ae0b1d.

--- exercises/exercise11/main.tf
+++ exercises/exercise11/main.tf
@@ -89,8 +89,9 @@ resource "aws_lambda_permission" "allow_api_gateway" {
   statement_id  = "AllowAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.my_lambda.function_name
-  qualifier     = "1"
+  qualifier     = aws_lambda_function.my_lambda.version
   principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 }
 
 resource "aws_api_gateway_deployment" "api_deployment" {
